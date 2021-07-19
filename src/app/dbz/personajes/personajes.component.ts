0e1c65cbf70f28d6c2b9f0a732a4c06c188ffdf1
Sebastian Asis <sasisromero10@gmail.com>
2021-07-19 10:53:36 -0500

    :dart: IMPROVE: @input

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css']
+})
+export class PersonajesComponent {
+
+  // viene desde el padre
+  @Input() personajes:Personaje[] = []
+
+}
