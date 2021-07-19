68a3ba5d91adc22346e0d81738777d4f37401710
Sebastian Asis <sasisromero10@gmail.com>
2021-07-19 10:37:48 -0500

    :dart: IMPROVE: forms in angular

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string
+  poder: number
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  personajes: Personaje[] = [
+    {
+      nombre:'Goku',
+      poder: 15000
+    },
+    {
+      nombre:'Vegeta',
+      poder: 7500
+    }
+  ]
+  nuevo: Personaje = {
+    nombre:'',
+    poder: 0
   }
+
+  agregar(){
+    if( this.nuevo.nombre.trim().length === 0){
+      alert('Nombre vacio')
+      return;
+    }
+    if(this.nuevo.poder <= 0) {
+      alert('Poder invalido')
+      return
+    }
+    console.log(this.nuevo)
+    this.personajes.push(this.nuevo)
+    this.nuevo = {
+      nombre:'',
+      poder:0
+    }
+    alert('Personaje agregado con exito')
+  }
+
+  // cambiarNombre(e:any){
+  //   this.nuevo.nombre = e.target.value || ''
+  // }
 
 }
